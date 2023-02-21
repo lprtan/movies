f5febfeb41645844bb5615d481f45fdeb012413b
lprtan <lprntan@gmail.com>
2023-02-21 19:33:47 +0300

    ..

--- movies/Controllers/MoviesController.cs
+++ movies/Controllers/MoviesController.cs
@@ -13,6 +13,7 @@ namespace movies.Controllers
 {
     public class MoviesController : Controller
     {
+        [HttpGet]
         public async Task<IActionResult> IndexAsync()
         {
             List<Movie> m = new List<Movie>();
@@ -49,5 +50,46 @@ namespace movies.Controllers
 
             return View(m);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchMovies(string searchMovieName)
+        {
+            List<Movie> m = new List<Movie>();
+            string apiKey = "19cad3e4cec968d8a2934e5b2ed00d7a";
+            string baseUrl = "https://api.themoviedb.org/3/search/movie?query="+ searchMovieName + "&api_key="+apiKey;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await httpClient.GetAsync(baseUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+
+                        dynamic data = JObject.Parse(json);
+                        var movies = data.results;
+
+                        foreach (var movie in movies)
+                        {
+                            var movieName = movie.original_title;
+                            var pop = movie.popularity;
+                            var date = movie.release_date;
+                            var rate = movie.vote_average;
+
+                            m.Add(new Movie { movieName = movieName, popularite = pop, date = date, rate = rate });
+
+                        }
+                    }
+                }
+            }
+
+            return View(m);
+        }
+
+
+
     }
 }
